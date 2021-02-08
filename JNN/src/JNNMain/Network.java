bbe7632e5ebda8e0a703b90c031e915518e37734
JanVanDerWall <janvdwall12@gmail.com>
2021-02-08 22:06:00 +0100

    Added the commons Math Library and added the Network Class with it's attributes and the "calculate" function

--- JNN/src/JNNMain/Network.java
+++ JNN/src/JNNMain/Network.java
@@ -1,5 +1,25 @@
 package JNNMain;
+import org.apache.commons.math3.linear.*;
+
+
+//Dies ist die Network Klasse, die repräsentiert ein Neuronales Netz.
+//Sie enthält alle Informationen über das Netz, wie die Layer und deren Größe,
+//die Bias und Weight Werte
 
 public class Network {
+	
+	RealVector[] biases;
+	RealMatrix[] weights;
+	int numberOfLayers;
+	int[] layerSizes;
+	
+	public Network(int[] layers) {
+		
+	}
+	
+	
+	public int[] calculate() {
+		return null;
+	}
 
 }
