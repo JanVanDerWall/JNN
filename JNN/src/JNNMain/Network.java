02643b421d93a588c8e33c0d479dc61f0fbe3c48
JanVanDerWall <janvdwall12@gmail.com>
2021-02-09 01:05:38 +0100

    Added constructor to "Network"

--- JNN/src/JNNMain/Network.java
+++ JNN/src/JNNMain/Network.java
@@ -1,4 +1,6 @@
 package JNNMain;
+import java.util.Random;
+
 import org.apache.commons.math3.linear.*;
 
 
@@ -15,11 +17,34 @@ public class Network {
 	
 	public Network(int[] layers) {
 		
+		biases = new RealVector[layers.length-1];
+		weights = new RealMatrix[layers.length-1];
+		
+		for (int i = 0; i < layers.length-1; i++) {
+			Random r = new Random();
+			double[] biasValues = new double[layers[i+1]];
+			for (int j = 0; j < biasValues.length; j++) {
+				biasValues[j] = r.nextDouble();
+			}
+			biases[i] = new ArrayRealVector(biasValues);
+			double[][] weightValues =  new double[layers[i+1]][layers[i]];
+			for (int j = 0; j < weightValues.length; j++) {
+				for (int j2 = 0; j2 < weightValues[j].length; j2++) {
+					weightValues[j][j2] = r.nextDouble();
+				}
+			}
+			weights[i] = new Array2DRowRealMatrix(weightValues);
+		}
+		
 	}
 	
 	
 	public int[] calculate() {
 		return null;
+	}
+	
+	private double sigmoid (double input) {
+		return (1.0/(1.0+ Math.exp(input)));
 	}
 
 }
