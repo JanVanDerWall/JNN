b10d8d15eec642b210d7071d8f4a4acbed2ff416
Jan van der Wall <nerv.nerv23@gmail.com>
2021-02-11 19:01:56 +0100

    Added trainDradientDeccent and backprop
    without implementation

--- JNN/src/JNNMain/Lerner.java
+++ JNN/src/JNNMain/Lerner.java
@@ -1,10 +1,33 @@
 package JNNMain;
 
+import org.apache.commons.math3.linear.*;
+
 public class Lerner {
 	
 	Network network;
 	TrainDataSet[] trainData;
 	
 	double lerningRate;
+	
+	public Network trainGradientDecent() {
+		return trainGradientDecent(trainData);
+	}
+	
+	public Network trainGradientDecent(TrainDataSet[] training) {
+		return null;
+	}
+	
+	private Gradient backprop() {
+		return null;
+	}
 
+}
+
+class Gradient{
+	final RealVector[] bias_g;
+	final RealMatrix[] weight_g;
+	public Gradient(RealVector[] b, RealMatrix[] w){
+		bias_g = b;
+		weight_g = w;
+	}
 }
